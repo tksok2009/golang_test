95d7b91993ce0a5dff809ed16f81ef8ebb5a2026
admin <admin@admin-no-MacBook-Pro.local>
2016-08-15 06:49:20 +0900

    refs #2 variables

---
+++ lessons/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	var a = 1
+	var b string = "string"
+	c := "string short case" //stringを短く指定する場合
+
+	var d,e int = 3,4
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d+e)
+
+
+}
